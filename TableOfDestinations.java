b4394c1c51cabc9c7b0c2332c1fd8259c3861fce
aimiktena <117830508+aimiktena@users.noreply.github.com>
2022-11-10 15:09:45 +0200

    Create TableOfDestinations.java

---
+++ TableOfDestinations.java
@@ -0,0 +1,9 @@
+public class TableOfDestinations {
+    public String[] tableDest = new tableDest[20];
+    public void appearsTableOfDestinations {
+        tableDest[] = {"1.Athens", "2.Paris", "3.Rome", "4.London", "5.Barcelona", "6.Prague", "7.Vienna", "8.Amstedram", "9.Budapest", "10.Lisbon", "11.Copenhagen", "12.Istanbul", "13.Berlin", "14.Stockholm", "15.Dublin", "16.Oslo", "17.Milan", "18.Bucharest", "19.Moscow", "20.Madrid"}
+        for(int i = 0 ; i < 20 ; i++) {
+            System.out.println(tableDest[i+1]);
+        }
+    }
+}
