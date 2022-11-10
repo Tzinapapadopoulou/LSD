376380c0b5c7a17f1e837c01b842771a7d0a8a0a
aimiktena <117830508+aimiktena@users.noreply.github.com>
2022-11-10 13:42:47 +0200

    Update Executable.java

--- Executable.java
+++ Executable.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class Executable {
-    //5 χρήστες , 2 στοιχεία ταξιδιού (ημερομηνία,προορισμός)//
+    //5 χρήστες//
     public String[] tripDest = new tripDest[5];
     public int[] tripMonth = new tripMonth[5];
 }
