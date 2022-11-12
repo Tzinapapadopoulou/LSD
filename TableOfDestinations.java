cc1532ab7b4c126ceaffe182ed723b7a6f4a74dc
aimiktena <117830508+aimiktena@users.noreply.github.com>
2022-11-12 13:05:54 +0200

    Update TableOfDestinations.java

--- TableOfDestinations.java
+++ TableOfDestinations.java
@@ -1,7 +1,26 @@
 public class TableOfDestinations {
-    public String[] tableDest = new tableDest[20];
-    public void appearsTableOfDestinations {
-        tableDest[] = {"1.Athens", "2.Paris", "3.Rome", "4.London", "5.Barcelona", "6.Prague", "7.Vienna", "8.Amstedram", "9.Budapest", "10.Lisbon", "11.Copenhagen", "12.Istanbul", "13.Berlin", "14.Stockholm", "15.Dublin", "16.Oslo", "17.Milan", "18.Bucharest", "19.Moscow", "20.Madrid"}
+    public void appearsTableOfDestinations () {
+        String[] tableDest = new String[20];
+        tableDest[0] = ("1.Athens");
+        tableDest[1] = ("2.Paris");
+        tableDest[2] = ("3.Rome");
+        tableDest[3] = ("4.London");
+        tableDest[4] = ("5.Barcelona");
+        tableDest[5] = ("6.Prague");
+        tableDest[6] = ("7.Vienna");
+        tableDest[7] = ("8.Amsterdam");
+        tableDest[8] = ("9.Budapest");
+        tableDest[9] = ("10.Lisbon");
+        tableDest[10] = ("11.Copenagen");
+        tableDest[11] = ("12.Istanbul");
+        tableDest[12] = ("13.Berlin");
+        tableDest[13] = ("14.Stockholm");
+        tableDest[14] = ("15.Dublin");
+        tableDest[15] = ("16.Oslo");
+        tableDest[16] = ("17.Milan");
+        tableDest[17] = ("18.Bucharest");
+        tableDest[18] = ("19.Moscow");
+        tableDest[19] = ("20.Madrid");
         for(int i = 0 ; i < 20 ; i++) {
             System.out.println(tableDest[i+1]);
         }
