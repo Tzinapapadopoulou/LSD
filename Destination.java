a8e194aa2e985abf611fff954672af35c94705c1
aimiktena <117830508+aimiktena@users.noreply.github.com>
2022-11-12 13:04:25 +0200

    Update Destination.java

--- Destination.java
+++ Destination.java
@@ -1,24 +1,48 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.InputMismatchException;
 
 public class Destination extends Executable {
     //κληρονομεί τον πίνακα από την κλάση Executable με τα στοιχεία των χρηστών ο οποίος είναι δηλωμένος ως public έξω από τισ μεθόδους τησ κλάσης//
     //υποθέτουμε ότι ο πίνακας που κληρονομείται είναι 5(χρήστες)*5(στοιχεία)//
     //κληρονομείται και δημόσια μεταβλητή τύπου int και όνομα position με την θέση του εκάστοτε χρήστη στον πίνακα με τα στοιχεία// 
+    Scanner input = new Scanner(System.in);
     public void fillArray() {
         //μέθοδο που συμπληρώνει τα στοχεία του ταξιδιού του χρήστη που βρίσκεται στην θέση position//
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter month of your next trip :");
-        int month = input.nextInt();
-        checkMonth(int month);
+        TableOfDestinations a = new TableOfDestinations();
+        boolean continueLoop = true;
+        int month = 0;
+        do {
+            try {
+                //κάνουμε έλεγχο για το αν εισάγεται int //
+                System.out.println("Enter month of your next trip :");
+                month = input.nextInt();
+                continueLoop = false;
+            } catch (InputMismatchException e) {
+                input.nextLine();
+                System.out.println("Please enter an integer");
+            }   
+        } while (continueLoop);
+        checkMonth(month);
         super.tripMonth[super.position]  = month;
-        TableOfDestinations.appearsTableOfDestinations();
+        a.appearsTableOfDestinations();
         System.out.println("Choose the destination of your next trip from the list above (Enter number)");
-        int numOfDest = input.nextInt();
-        checkDestination(int numOfDest);
+        continueLoop = true;
+        int numOfDest = 0;
+        do {
+            try {
+                //κάνουμε έλεγχο για το αν εισάγεται int //
+                System.out.println("Enter number of your next destination :");
+                numOfDest = input.nextInt();
+                continueLoop = false;
+            } catch (InputMismatchException e) {
+                input.nextLine();
+                System.out.println("Please enter an integer");
+            }   
+        } while (continueLoop);
+        checkDestination(numOfDest);
         super.tripDest[super.position] = numOfDest;
     }
-    public void checkMonth(int a) {
+    public int checkMonth(int a) {
         //γίνεται έλεγχος εγκυρότητας του μήνα//
         if(a < 1 && a > 12) {
             do{
@@ -26,13 +50,16 @@ public class Destination extends Executable {
                  a = input.nextInt();
             }while (a < 1 && a > 12); 
         }
+        return a;
     }
-    public void checkDestination(int b) {
+    public int checkDestination(int b) {
         //γίνεται έλεγχος εγκυρότητας του προορισμού//
         if(b < 1 && b > 20) {
-        do {
-            System.out.println("invalid number of destination, please try again");
-            b = input.nextInt();
-        }while (b < 1 && b > 20);
+            do {
+              System.out.println("invalid number of destination, please try again");
+              b = input.nextInt();
+            }while (b < 1 && b > 20);
+        }
+        return b;
     }
 }
