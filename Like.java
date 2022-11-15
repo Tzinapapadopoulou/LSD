dc31c6eb139e8ff57c0addb486afc0fbf10396ef
annapetropoulou <117905821+annapetropoulou@users.noreply.github.com>
2022-11-15 14:58:54 +0200

    Add files via upload

--- Like.java
+++ Like.java
@@ -1,17 +1,24 @@
 import java.util.Stack;
 
 public class Like {
-    // την μέθοδο like την καλούμε για κάθε χρήστη που κάνει λαικ
-    int[] likesarray = new int[10];
-    int[][] wholikedwho = new int[10][10];
-    for (int i = 0; i < wholikedwho.length; i++) {
-        for (int j = 0; j < wholikedwho[0].length; j++) {
-            wholikedwho[i][j] = 0;
-        }
-    }
-    //int chatbudz[][] = new chatbudz[45][2];   // (n*(n-1))/2 , όπου n πλήθος των χρηστών με τον ίδιο προορισμό
 
+    public static int[][] createWholikedwho() {
+        int[][] wholikedwho = new int[10][10];
+        for (int i = 0; i < wholikedwho.length; i++) {
+            for (int j = 0; j < wholikedwho[0].length; j++) { 
+                wholikedwho[i][j] = 0; 
+            }
+        }
+        return wholikedwho;
+    }
     
+    public static int[] createLikesarray() {
+        int[] likesarray = new int[10];
+        return likesarray;
+    }
+    
+    
+    //int chatbudz[][] = new chatbudz[45][2];   // (n*(n-1))/2 , όπου n πλήθος των χρηστών με τον ίδιο προορισμό
 
     public static int[] addLike(int likesarray[], int userliked) {
         likesarray[userliked]++;
@@ -23,12 +30,17 @@ public class Like {
         return wholikedwho;
     }
 
+    public static Stack<Integer> createChatbudz() {
+        Stack<Integer> chatbudz = new Stack<Integer>(); 
+        return chatbudz;
+    }
+    
     public static Stack<Integer> mutualLike(int userliker, int userliked, int wholikedwho[][], Stack<Integer> chatbudz) {
         if ( (wholikedwho[userliker][userliked] == wholikedwho[userliked][userliker] ) && ( wholikedwho[userliker][userliked] != 2) ) {
             chatbudz.push(userliker);
             chatbudz.push(userliked);
-            return chatbudz;
         }
+        return chatbudz;
     }
     
     public static int[][] setToTwo(int wholikedwho[][], int userliked, int userliker) {
@@ -36,9 +48,14 @@ public class Like {
         wholikedwho[userliked][userliker] = 2;
         return wholikedwho;
     }
-    
+
     public static void main(String[] args) {
-        Stack<Integer> chatbudz = new Stack<Integer>(); 
-        
+        createWholikedwho();
+        createLikesarray();
+        createChatbudz();
+        addLike(null, 0);
+        isTrue(null, 0, 0);
+        mutualLike(0, 0, null, null);
+        setToTwo(null, 0, 0);
     }
 }
