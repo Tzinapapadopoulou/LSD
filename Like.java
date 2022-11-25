a85e93bbe91c8576ad6005540f64bf71dfe3cee8
natakokota <115426600+natakokota@users.noreply.github.com>
2022-11-25 13:48:48 +0200

    Update Like.java

--- Like.java
+++ Like.java
@@ -11,30 +11,36 @@ public class Like {
         }
         return wholikedwho;
     }
-    
+    //intializing wholikedwho array
+
     public static int[] createLikesarray() {
         int[] likesarray = new int[10];
+        for (int i = 0; i < likesarray.length; i++) {
+            likesarray[i] = 0;
+        }
         return likesarray;
     }
+    //initializing likesarray
     
-    
-    //int chatbudz[][] = new chatbudz[45][2];   // (n*(n-1))/2 , όπου n πλήθος των χρηστών με τον ίδιο προορισμό
 
     public static int[] addLike(int likesarray[], int userliked) {
         likesarray[userliked]++;
         return likesarray;
     }
+    //adding likes
 
     public static int[][] isTrue(int wholikedwho[][], int userliker, int userliked) {
         wholikedwho[userliker][userliked] = 1;
         return wholikedwho;
     }
+    //2 users like eachother >> value 1 on wholikedwho
 
     public static Stack<Integer> createChatbudz() {
         Stack<Integer> chatbudz = new Stack<Integer>(); 
         return chatbudz;
     }
-    
+    //create couples to chat after liking eachother
+
     public static Stack<Integer> mutualLike(int userliker, int userliked, int wholikedwho[][], Stack<Integer> chatbudz) {
         if ( (wholikedwho[userliker][userliked] == wholikedwho[userliked][userliker] ) && ( wholikedwho[userliker][userliked] != 2) ) {
             chatbudz.push(userliker);
@@ -42,20 +48,24 @@ public class Like {
         }
         return chatbudz;
     }
+    //pushing in stack mutualLike the liker first then the liked
     
     public static int[][] setToTwo(int wholikedwho[][], int userliked, int userliker) {
         wholikedwho[userliker][userliked] = 2;
         wholikedwho[userliked][userliker] = 2;
         return wholikedwho;
     }
+    //stating in wholikedwho array that the couple was made and now they can chat
 
     public static void main(String[] args) {
-        createWholikedwho();
-        createLikesarray();
-        createChatbudz();
-        addLike(null, 0);
-        isTrue(null, 0, 0);
-        mutualLike(0, 0, null, null);
-        setToTwo(null, 0, 0);
+        int[][] wholikedwho = createWholikedwho();
+        int[] likesarray = createLikesarray();
+        Stack<Integer> chatbudz = createChatbudz();
+        addLike(likesarray, 0);
+        isTrue(wholikedwho, 0, 0);
+        mutualLike(0, 0, wholikedwho, chatbudz);
+        setToTwo(wholikedwho, 0, 0);
     }
 }
+
+    //int chatbudz[][] = new chatbudz[45][2];   // (n*(n-1))/2 , όπου n πλήθος των χρηστών με τον ίδιο προορισμό
