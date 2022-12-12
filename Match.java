41bdc6480460e190855750691091d6807a03ab1e
Maria Fountoulaki <116514808+t8210157@users.noreply.github.com>
2022-12-12 13:51:42 +0200

    Update Match.java

--- Match.java
+++ Match.java
@@ -1,35 +1,59 @@
-import java.util.ArrayList;
-import java.util.ListIterator;
-import java.util.HashMap;
-
-import javafx.beans.binding.ListBinding;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 
 public class Match extends Executable {
     //κληρονομείται δημόσια μεταβλητή τύπου int (position) η οποία δηλώνεται στην κλάση Executable η οποίαδείχνει την θέση που κατέχει ο συγκεκριμένος χρήστης στον πίνακα με τα δεδομένα(tableOfAllUsers)//
     //κληρονομείται ακόμα ο πίνακας με τα δεδομένα όλων των χρηστών τον θεωρούμε προσωρινά 7(χαρακτηριστικά)*5(χρήστες)(tableOfAllUsers)(από την Registration) και ο παράλληλος πίνακας 5 γραμμων με τουσ μήνες(tableMonth) και ο παράλληλος πίνακας 5 γραμμών με τους προορισμούς(tableDest)(από την Executable)//
     public String matchingUsersProfile() {
         //το i είναι ο αριθμός των χρηστών//
+        ConnectDB connection = new ConnectDB();
+        Connection con = null;
+        PreparedStatement p1 = null;
+        PreparedStatement p2 = null;
+        ResultSet rs1 = null;
+        ResultSet rs2 = null;
+
+        con = connection.connectionDB();
+        Statement stmt = con.createStatement();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnsize = rsmd.getPrecision(1);
+
         boolean thereIsAtLeastABuddy = false;
-        ListIterator<Integer> tableDestIterator = tableDest.tableDestIterator();
-        ListIterator<Integer> tableMonthIterator = tableMonth.tableMonthIterator();
-        int i = 0;
-        while(tableDestIterator.hasNext() && tableMonthIterator.hasNext()) {
-            if(super.tableDest.get(super.position) == super.tableDest.get(i) && super.tableMonth.get(super.position) == super.tableMonth.get(i) && i != super.position) {
-                //υποθέτω οτι στον πίνακα με τα στοιχεία των χρηστών το username βρίσκεται στην στήλη 3 με την σειρά που τα έχετε βάλει στην κλάση Registration//
-                if (thereIsAtLeastABuddy == false) {
-                    System.out.println("Your possible travel Buddies are :");
-                    thereIsAtLeastABuddy = true;
+        list <Integer> allowlike = new ArrayList<Integer>();
+
+        try {
+            String sqlfortheuser = "select DEST, MONTH from USERS where POSITION=" + Integer.toString(super.position);
+            p1 = con.prepareStatement(sqlfortheuser);
+            rs1 = p1.executeQuery();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+
+        for(int i = 0 ; i < super.columnsize; i++) {
+                try {
+                    String sql = "select DEST, MONTH from USERS where POSITION=" + Integer.toString(i);
+                    p2 = con.prepareStatement(sql);
+                    rs2 = p2.executeQuery();
+                    if(rs1.getInt("DEST") == rs2.getInt("DEST") && rs1.getInt("MONTH") == rs2.getInt("MONTH") && i != super.position) {
+                        if (thereIsAtLeastABuddy == false) {
+                            System.out.println("Your possible travel Buddies are :");
+                            thereIsAtLeastABuddy = true;
+                        }
+                        System.out.printf(rs2.getString("USERNAME"));
+                        allowlike.add(i);
+                    }
+                } catch (SQLException e) {
+                    System.out.println(e);
                 }
-                //υποθετουμε ότι η λίστα με τα στοιχεία των χρηστών (όνομα,username....) ονομάζεται ListOfAllUsers και είναι δύο διαστάσεων//
-                //θέλουμε να εμφανίζει το username του χρήστη στην γραμμή i//
-                System.out.printf("%s", super.ListOfAllUsers.get[i][3]);
-            }
-            i++;
+            
         }
         if(thereIsAtLeastABuddy == false) {
             System.out.println("We're sorry but there isn't any travel buddy for you. Please try another destination or month for your trip");
             Destination rearrangeTheTrip = new Destination();
-            rearrangeTheTrip.fillArrayList();
+            rearrangeTheTrip.fillArray();
         }
     }
 }
