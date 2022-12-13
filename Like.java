9bce69726fded8961c3395715bda6103efc902aa
natakokota <115426600+natakokota@users.noreply.github.com>
2022-12-13 20:10:31 +0200

    Update Like.java

--- Like.java
+++ Like.java
@@ -1,14 +1,14 @@
 import java.util.Stack;
 
 public class Like {
-
-    public static int[][] createWholikedwho() {
-        /*η κλαση Match απο την μεθοδο matchingUsersProfile θα στέλνει μία λίστα με ονομα allowlike με τα Primary key (στήλη POSITION του πίνακα sql) 
+    /*η κλαση Match απο την μεθοδο matchingUsersProfile θα στέλνει μία λίστα με ονομα allowlike με τα Primary key (στήλη POSITION του πίνακα sql) 
 των χρηστών με τουσ οποίουσ ο χρήστησ που είναι συνδεδεμένοσ τωρα (στην θέση super.position(βλέπε κλάση Excecutable) 
 κάνει ματσ. Ο πίνακας wholikewho θα είναι τετραγωνικόσ με αριθμό στηλών(και γραμμών) το columnsize του πίνακα τοθ 
 sql ππου περιέχει τα στοιχεία των χρηστών. πχ αν η λίστα : <12,14> τότε θα υπάρχει δυνατότητα like
  στισ θέσεις πίνακα [super.position,12] και  [super.position, 14]*/
-        int[][] wholikedwho = new int[10][10];
+
+    public static int[][] createWholikedwho() {
+        int[][] wholikedwho = new int[allowlike.length][allowlike.length];
         for (int i = 0; i < wholikedwho.length; i++) {
             for (int j = 0; j < wholikedwho[0].length; j++) { 
                 wholikedwho[i][j] = 0; 
@@ -18,20 +18,20 @@ sql ππου περιέχει τα στοιχεία των χρηστών. πχ 
     }
     //intializing wholikedwho array
 
-    public static int[] createLikesarray() {
-        int[] likesarray = new int[10];
-        for (int i = 0; i < likesarray.length; i++) {
-            likesarray[i] = 0;
-        }
-        return likesarray;
-    }
+  //  public static int[] createLikesarray() {
+   //     int[] likesarray = new int[];
+   //     for (int i = 0; i < likesarray.length; i++) {
+    //        likesarray[i] = 0;
+    //    }
+     //   return likesarray;
+   // }
     //initializing likesarray
     
 
-    public static int[] addLike(int likesarray[], int userliked) {
-        likesarray[userliked]++;
-        return likesarray;
-    }
+   // public static int[] addLike(int likesarray[], int userliked) {
+   //     likesarray[userliked]++;
+   //     return likesarray;
+   // }
     //adding likes
 
     public static int[][] isTrue(int wholikedwho[][], int userliker, int userliked) {
@@ -63,14 +63,18 @@ sql ππου περιέχει τα στοιχεία των χρηστών. πχ 
     //stating in wholikedwho array that the couple was made and now they can chat
 
     public static void main(String[] args) {
+        Match objMatch = new Match();
+        int[] allowlike = objMatch.matchingUsersProfile();
+
         int[][] wholikedwho = createWholikedwho();
-        int[] likesarray = createLikesarray();
+        //int[] likesarray = createLikesarray();
         Stack<Integer> chatbudz = createChatbudz();
-        addLike(likesarray, 0);
+        //addLike(likesarray, 0);
         isTrue(wholikedwho, 0, 0);
         mutualLike(0, 0, wholikedwho, chatbudz);
         setToTwo(wholikedwho, 0, 0);
+        
+
+
     }
 }
-
-    //int chatbudz[][] = new chatbudz[45][2];   // (n*(n-1))/2 , όπου n πλήθος των χρηστών με τον ίδιο προορισμό
