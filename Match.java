1797436ba7af4efff0120da402636724cd8d307f
aimiktena <117830508+aimiktena@users.noreply.github.com>
2023-01-11 00:59:44 +0200

    Update Match.java

--- Match.java
+++ Match.java
@@ -1,17 +1,17 @@
+package match;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.*;
+import java.sql.*;
 
-public class Match extends Executable {
-    // κληρονομείται δημόσια μεταβλητή τύπου int (position) η οποία δηλώνεται στην
-    // κλάση Executable η οποίαδείχνει την θέση που κατέχει ο συγκεκριμένος χρήστης
-    // στον πίνακα με τα δεδομένα(tableOfAllUsers)//
-    // κληρονομείται ακόμα ο πίνακας με τα δεδομένα όλων των χρηστών τον θεωρούμε
-    // προσωρινά 7(χαρακτηριστικά)*5(χρήστες)(tableOfAllUsers)(από την Registration)
-    // και ο παράλληλος πίνακας 5 γραμμων με τουσ μήνες(tableMonth) και ο παράλληλος
-    // πίνακας 5 γραμμών με τους προορισμούς(tableDest)(από την Executable)//
+public class Match {
+    public int position;
+    public void setPosition(int a){
+		position = a;
+    }
     public ArrayList<Integer> matchingUsersProfile() {
         // το i είναι ο αριθμός των χρηστών//
         ConnectDB connection = new ConnectDB();
@@ -20,40 +20,47 @@ public class Match extends Executable {
         PreparedStatement p2 = null;
         ResultSet rs1 = null;
         ResultSet rs2 = null;
-
-        con = connection.connectionDB();
-        Statement stmt = con.createStatement();
-        ResultSetMetaData rsmd = rs.getMetaData();
-        int i = 0;
-        
-        boolean thereIsAtLeastABuddy = false;
         list<Integer> allowlike = new ArrayList<Integer>();
+        try{
+            con = connection.connectionDB();
+            Statement stmt = con.createStatement();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int i = 0;
 
-        try {
-            String sqlfortheuser = "select DEST, MONTH from USERS where POSITION=" + Integer.toString(super.position);
-            p1 = con.prepareStatement(sqlfortheuser);
-            rs1 = p1.executeQuery();
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        while (rsmd.next()) {
+            boolean thereIsAtLeastABuddy = false;
+
             try {
-                String sql = "select DEST, MONTH from USERS where POSITION=" + Integer.toString(i);
-                p2 = con.prepareStatement(sql);
-                rs2 = p2.executeQuery();
-                if (rs1.getInt("DEST") == rs2.getInt("DEST") && rs1.getInt("MONTH") == rs2.getInt("MONTH")
-                        && i != super.position) {
-                    if (thereIsAtLeastABuddy == false) {
-                        System.out.println("Your possible travel Buddies are :");
-                        thereIsAtLeastABuddy = true;
-                    }
-                    System.out.printf(rs2.getString("USERNAME"));
-                    allowlike.add(i);
-                }
+                String sqlfortheuser = "select DEST, MONTH from USERS where POSITION=" + Integer.toString(position);
+                p1 = con.prepareStatement(sqlfortheuser);
+                rs1 = p1.executeQuery();
             } catch (SQLException e) {
                 System.out.println(e);
             }
-            i++;
+            while (rsmd.next()) {
+                try {
+                    String sql = "select DEST, MONTH from USERS where POSITION=" + Integer.toString(i);
+                    p2 = con.prepareStatement(sql);
+                    rs2 = p2.executeQuery();
+                    if (rs1.getInt("DEST") == rs2.getInt("DEST") && rs1.getInt("MONTH") == rs2.getInt("MONTH")
+                            && i != position) {
+                        if (thereIsAtLeastABuddy == false) {
+                            System.out.println("Your possible travel Buddies are :");
+                            thereIsAtLeastABuddy = true;
+                        }
+                        System.out.printf(rs2.getString("USERNAME"));
+                        allowlike.add(i);
+                    }
+                } catch (SQLException e) {
+                    System.out.println(e);
+                }
+                i++;
+            }
+            rs1.close();
+            rs2.close();
+            p1.close();
+            p2.close();
+        }catch (SQLException e){
+            System.out.println("SQLException :");
         }
         if (thereIsAtLeastABuddy == false) {
             System.out.println(
