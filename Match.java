9eaca08406c21ec7178b3f9b3d8aee4558de8429
Tzinapapadopoulou <117903616+Tzinapapadopoulou@users.noreply.github.com>
2023-01-11 11:26:48 +0200

    Update Match.java

--- Match.java
+++ Match.java
@@ -1,74 +1,78 @@
-package match;
+package net.codejava.sql;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
 import java.util.*;
 import java.sql.*;
 
 public class Match {
-    public int position;
+    public static int position;
+    
     public void setPosition(int a){
 		position = a;
     }
-    public ArrayList<Integer> matchingUsersProfile() {
-        // το i είναι ο αριθμός των χρηστών//
-        ConnectDB connection = new ConnectDB();
-        Connection con = null;
-        PreparedStatement p1 = null;
-        PreparedStatement p2 = null;
-        ResultSet rs1 = null;
-        ResultSet rs2 = null;
-        list<Integer> allowlike = new ArrayList<Integer>();
+    
+    public static boolean thereIsAtLeastABuddy = false;
+    public static ArrayList<Integer> matchingUsersProfile() throws ClassNotFoundException {
+      
+        List<Integer> allowlike = new ArrayList<Integer>();
+        
         try{
-            con = connection.connectionDB();
-            Statement stmt = con.createStatement();
-            ResultSetMetaData rsmd = rs.getMetaData();
-            int i = 0;
-
-            boolean thereIsAtLeastABuddy = false;
+        	Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			String url = "jdbc:sqlserver://" +"TZINA-PC" + ":1433;DatabaseName=" + "TRABUDZ" + ";encrypt=true;trustServerCertificate=true";
+            String user = "LSD";
+            String passWord = "LSD123";
+            Connection conn = DriverManager.getConnection(url, user, passWord);
+            System.out.println("connected successfully to Database");
+            Statement stmt1 = conn.createStatement();
+            Statement stmt2 = conn.createStatement();
+            ResultSet rs1= null;
+            ResultSet rs2 = null;
+           
+            int i = 1;
 
             try {
-                String sqlfortheuser = "select DEST, MONTH from USERS where POSITION=" + Integer.toString(position);
-                p1 = con.prepareStatement(sqlfortheuser);
-                rs1 = p1.executeQuery();
+                String sqlfortheuser = "select DEST, MONTHY from USERS where POSITION=" + String.valueOf(position);
+                rs1 = stmt1.executeQuery(sqlfortheuser);
+                System.out.println("ok1");
+                while (((ResultSet) rs1).next()) {
+                    try {
+                        String sql = "select DEST, MONTHY from USERS where POSITION=" + String.valueOf(i);
+                        rs2 = stmt2.executeQuery(sql);
+                        System.out.println("ok2");
+                        if (rs1.getInt("DEST") == rs2.getInt("DEST") && rs1.getInt("MONTHY") == rs2.getInt("MONTHY")
+                                && i != position) {
+                            if (thereIsAtLeastABuddy == false) {
+                                System.out.println("Your possible travel Buddies are :");
+                                thereIsAtLeastABuddy = true;
+                            }
+                            System.out.printf(rs2.getString("USERNAME"));
+                            allowlike.add(i);
+                        }
+                    } catch (SQLException e) {
+                        System.out.println(e);
+                    }
+                    i++;
+                }
+                conn.close();
             } catch (SQLException e) {
                 System.out.println(e);
             }
-            while (rsmd.next()) {
-                try {
-                    String sql = "select DEST, MONTH from USERS where POSITION=" + Integer.toString(i);
-                    p2 = con.prepareStatement(sql);
-                    rs2 = p2.executeQuery();
-                    if (rs1.getInt("DEST") == rs2.getInt("DEST") && rs1.getInt("MONTH") == rs2.getInt("MONTH")
-                            && i != position) {
-                        if (thereIsAtLeastABuddy == false) {
-                            System.out.println("Your possible travel Buddies are :");
-                            thereIsAtLeastABuddy = true;
-                        }
-                        System.out.printf(rs2.getString("USERNAME"));
-                        allowlike.add(i);
-                    }
-                } catch (SQLException e) {
-                    System.out.println(e);
-                }
-                i++;
-            }
-            rs1.close();
-            rs2.close();
-            p1.close();
-            p2.close();
+           
         }catch (SQLException e){
             System.out.println("SQLException :");
         }
         if (thereIsAtLeastABuddy == false) {
-            System.out.println(
-                    "We're sorry but there isn't any travel buddy for you. Please try another destination or month for your trip");
-            Destination rearrangeTheTrip = new Destination();
-            rearrangeTheTrip.fillArray();
+            System.out.println("We're sorry but there isn't any travel buddy for you. Please try again with new destination info");
+            
+            return (ArrayList<Integer>) allowlike;
         }else {
-            return allowlike;
+            return (ArrayList<Integer>) allowlike;
         }
+        
     }
+    
+    public boolean getBoolean() {
+    	return thereIsAtLeastABuddy;
+    }
+    
+  
 }
