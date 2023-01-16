973a6630a453c9ba97b61323ee233bd00be3b628
aimiktena <117830508+aimiktena@users.noreply.github.com>
2023-01-16 15:06:00 +0200

    Delete Executable.java

--- Executable.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Executable {
-    //5 χρήστες//
-    ArrayList<Integer> tripDest = new ArrayList<Integer>();
-    ArrayList<Integer> tripMonth = new ArrayList<Integer>();
-    public int position;
-}
