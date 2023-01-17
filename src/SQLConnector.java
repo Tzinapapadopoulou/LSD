6487756f7760e70f4e53dbba390741e4929fff9c
Tzinapapadopoulou <117903616+Tzinapapadopoulou@users.noreply.github.com>
2023-01-17 13:44:24 +0200

    Classes

--- src/SQLConnector.java
+++ src/SQLConnector.java
@@ -7,7 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement
+import java.sql.Statement;
 
 
 public class SQLConnector {
