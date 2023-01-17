638cf5afa937ea12d06d6b8f69343f4c38751721
mariasigala <117905855+mariasigala@users.noreply.github.com>
2023-01-17 15:04:15 +0200

    Update Chat_Client.java

--- src/Chat_Client.java
+++ src/Chat_Client.java
@@ -1,12 +1,10 @@
 package net.codejava.sql;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 //package chatfromvideo;
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
